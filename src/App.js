74a18cad517b4e39a125b426a58cdd1e30ead132
bbogum19 <berkaye2001@hotmail.com>
2023-01-23 13:48:38 +0300

    A loading spinner has been added.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import React, { useState } from 'react';
-import { View, Button, FlatList } from 'react-native';
+import { View, Button, FlatList, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 
 import UserCard from './components/UserCard';
 
+const URL = 'https://jsonplaceholder.typicode.com/users';
+
 export default function App() {
 
   const [userList, setUserList] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const renderUser = ({ item }) => <UserCard name={item.name} email={item.email} username={item.username} />
 
   const fetchData = async () => {
-
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-    console.log("deneme");
-    setUserList(response.data)
-
+    setLoading(true);
+    const response = await axios.get(URL);
+    setLoading(false);
+    setUserList(response.data);
   }
 
   return (
     <View style={{ marginTop: 40 }}>
-      <FlatList
-        data={userList}
-        renderItem={renderUser}
-      />
+      {
+        loading
+          ? <ActivityIndicator size="large" />
+          : <FlatList
+            data={userList}
+            renderItem={renderUser}
+          />
+      }
       <Button title='Fetch Data' onPress={fetchData} />
     </View>
   );
 }
+
