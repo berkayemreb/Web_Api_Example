8d4ec1be1c3aa07beb237ed0a7516d38502dfddf
bbogum19 <berkaye2001@hotmail.com>
2023-01-23 11:06:08 +0300

    Then and catch topic have been completed.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ export default function App() {
 
   const fetchData = () => {
 
-    const response = axios.get('https://jsonplaceholder.typicode.com/users')
-    console.log(response);
-
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
   }
 
   return (
@@ -26,4 +26,8 @@ axios.get islemi yaparken gelen veri bir PROMISE yapısı olarak gelir.
 Promise yapıları asenkron yapılardır. Yani sunucuya istek atarız ve geri yanıt gelene kadar bu işlemin bitmesini beklemeyiz ( süresi ne zaman biterde veri gelir belli değiş çünkü)
 veriler gelene kadar kullanıcıyı baska bir arayüzde karsılarız. Bu olaya asenkron yapılar denir ve promise yapıları buna örnektir.
 
+                               -Then && Catch yapısı-
+Sunucuya istek atma işlemi (axios.get) yapıldıktan sonra kod sırası then ve catchi görmeden kaldığı yerden devam eder. Ne zaman istek cevabı gelirse o zaman then veya catch kısmı sıraya dahil edilir ve calısır (asenkron islemi)
+Eğer ki istek sonucu bir cevap gelirse (her sey yolunda) then bloğu calısır
+Eğer ki istek esnasında bir problem varsa (internetten kaynaklı, sunucudan kaynaklı vs.) catch kısmı calısır ve hata mesajını yakalayabiliriz.  
 */
