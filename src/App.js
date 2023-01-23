0139ffedf0d8eafefe3236b71ffbefe01b982cdd
bbogum19 <berkaye2001@hotmail.com>
2023-01-23 11:57:33 +0300

    Created UserCard component.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import axios from 'axios';
 export default function App() {
 
   const fetchData = async () => {
-
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-      console.log(response);
-
-    } catch (error) {
-      console.log("error verdi: ", error);
-    }
-
+    null;
   }
 
   return (
@@ -21,20 +13,3 @@ export default function App() {
     </View>
   );
 }
-
-/*            
-                            -- AXIOS ile ilgili not --  
-axios.get islemi end point'ten (server) data çeker, axios.post ise end point'e data gönderir.
-axios.get islemi yaparken gelen veri bir PROMISE yapısı olarak gelir.
-
-                                -Nedir bu Promise-
-Promise yapıları asenkron yapılardır. Yani sunucuya istek atarız ve geri yanıt gelene kadar bu işlemin bitmesini beklemeyiz ( süresi ne zaman biterde veri gelir belli değiş çünkü)
-veriler gelene kadar kullanıcıyı baska bir arayüzde karsılarız. Bu olaya asenkron yapılar denir ve promise yapıları buna örnektir.
-
-                               -Async && Await yapısı-
-Asenkron olan islemi senkron yapmak için async && await yapısını kullanırız. Yani bir yere istek attıktan sonra oradan cevap gelene kadar diğer işlemleri yapma ve bekle anlamına gelir.
-fonksiyonu async olarak tanımlamadan await kullanamazsın. 
-Then catch ile async await yapısını karsılastıramazsın cunku ihtiyaca göre her ikisi de kendi içinde avantajları vardır.
-Async await yapısında hata olursa eğer onu yakalamak istersek try catch kod bloğunu kullanabiliriz.                                
-
-*/
---
+++ src/components/UserCard/UserCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from 'react-native';
+import styles from './UserCard.style';
+
+const UserCard = (props) => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default UserCard;
