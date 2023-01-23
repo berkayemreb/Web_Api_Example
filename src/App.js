dd4ff13ce2cae18f75df9f82d90406d34a8e8c18
bbogum19 <berkaye2001@hotmail.com>
2023-01-23 13:17:23 +0300

    Data was retrieved from the web API and displayed on the screen.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, FlatList } from 'react-native';
 import axios from 'axios';
+
+import UserCard from './components/UserCard';
 
 export default function App() {
 
+  const [userList, setUserList] = useState([]);
+
+  const renderUser = ({ item }) => <UserCard name={item.name} />
+
   const fetchData = async () => {
-    null;
+
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log("deneme");
+    setUserList(response.data)
+
   }
 
   return (
     <View style={{ marginTop: 40 }}>
+      <FlatList
+        data={userList}
+        renderItem={renderUser}
+      />
       <Button title='Fetch Data' onPress={fetchData} />
     </View>
   );
--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import styles from './UserCard.style';
 
-const UserCard = (props) => {
+const UserCard = ({ name }) => {
     return (
-        <View>
-
+        <View style={styles.container}>
+            <Text style={styles.name}>{name}</Text>
         </View>
     )
 }
